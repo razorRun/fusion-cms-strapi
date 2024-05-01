28cc4af82ca26ed68f303a1108d8aa82ba71a84c
Roshan Milinda <roshan.milinda@accedo.tv>
2024-05-01 18:42:40 +1200

    GQL PG

--- config/plugins.js
+++ config/plugins.js
@@ -2,14 +2,11 @@ module.exports = ({ env }) => ({
   graphql: {
     enabled: true,
     config: {
-      endpoint: "/graphql",
+      endpoint: '/graphql',
       shadowCRUD: true,
       playgroundAlways: true,
       defaultLimit: 10,
       maxLimit: 20,
-      apolloServer: {
-        tracing: true,
-      },
-    },
-  },
+    }
+  }
 });
